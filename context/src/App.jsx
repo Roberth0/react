08c4d272eec3c748d257480232c9c34dec38ea72
Roberth <rjromerol1999@gmai.com>
2022-08-18 21:57:41 -0500

    Separated context component

--- context/src/App.jsx
+++ context/src/App.jsx
@@ -1,11 +1,35 @@
-import { memo } from 'react';
+import { createContext, useContext } from 'react';
+
+const Context = createContext('default value')
+
+const Provider = ({ children }) => {
+  return (
+    <Context.Provider value={"context value"}>
+      {children}
+    </Context.Provider>
+  )
+}
 
 function App() {
   return (
-    <div>
-      <p> Hey there this is a test</p>
-    </div>
+    <Provider>
+      <div>
+        <p> Hey there this is a test</p>
+        <p> Live server working </p>
+        <Section />
+      </div>
+    </Provider>
   );
+}
+
+const Section = ({ }) => {
+  const ctx = useContext(Context)
+  return (
+    <>
+      <div> Section Component </div>
+      <div> {ctx}</div>
+    </>
+  )
 }
 
 export default App;
