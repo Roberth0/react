dc92e7674ca307529b12f8a0e8ef34f0eb080bbc
Roberth <rjromerol1999@gmai.com>
2022-08-28 13:34:18 -0500

    dispatching different actions

--- redux/src/main.jsx
+++ redux/src/main.jsx
@@ -3,22 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { createStore } from 'redux'
 
-const store = createStore((state = "adf", action) => {
-  console.log({ state, action })
-  // action = {type: "typename", payload: any}
-  let { payload } = action
-  console.log("Estado actualizado ", payload);
-  state = payload;
-  return state;
+
+const store = createStore((state = 0, action) => {
+  let { type } = action;
+
+  switch (type) {
+    case 'incrementar': {
+      return state + 1
+    }
+    case 'decrementar': {
+      return state - 1
+    }
+    default:
+      return state
+  }
 })
 
-console.log(store);
-store.dispatch({ type: "estado1", payload: 10 })
-console.log(store.getState());
-store.dispatch({ type: "estado2", payload: 20 })
-console.log(store.getState());
-store.dispatch({ type: "estado3", payload: 30 })
-console.log(store.getState());
+store.dispatch({ type: 'incrementar' })
+store.dispatch({ type: 'incrementar' })
+store.dispatch({ type: 'incrementar' })
+console.log(store.getState())
+store.dispatch({ type: 'decrementar' })
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
