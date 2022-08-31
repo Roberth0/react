e8c71cc17220886d91179e15111eabe84cc26d3c
Roberth <rjromerol1999@gmai.com>
2022-08-30 22:36:34 -0500

    Item component

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
+import { useState } from 'react'
 
 const initialState = {
   entities: [],
@@ -6,32 +7,48 @@ const initialState = {
 
 
 export const reducer = (state = initialState, action) => {
-  let { type } = action
+  let { type, payload } = action
   switch (type) {
     case "todo/add":
-      console.log("reducer")
-      // return state.entities = [{}]
-      return { ...state, entities: [{}] }
+      return { ...state, entities: state.entities.concat(payload) }
   }
   return state
 }
+
+const Item = ({ todo }) => {
+  return (
+    <li>{todo.title}</li>
+  )
+}
+
 
 
 const App = () => {
   const dispatcher = useDispatch()
   const state = useSelector(x => x)
+  const [value, setValue] = useState("")
 
-  console.log(state, "renderizado")
+  const submit = e => {
+    e.preventDefault()
+
+    let todo = {
+      id: Math.random().toString(36),
+      title: value,
+    }
+    dispatcher({ type: "todo/add", payload: todo })
+    setValue("")
+  }
+  console.log({ state })
+
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Task description" />
+      <form onSubmit={submit}>
+        <input type="text" placeholder="Task description" value={value} onChange={e => setValue(e.target.value)} />
         <input type="submit" />
       </form>
-      <button onClick={() => dispatcher({ type: 'todo/add' })}>Add todo</button>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
+        {state ? state.entities.map(todo => <Item key={todo.id} todo={todo} />) : null}
       </ul>
     </div>
   )
