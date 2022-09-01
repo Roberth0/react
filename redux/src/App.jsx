7960b2dd1d288af24a83aa00f574f76908b08a09
Roberth <rjromerol1999@gmail.com>
2022-08-31 22:30:37 -0500

    redux update state

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -11,13 +11,27 @@ export const reducer = (state = initialState, action) => {
   switch (type) {
     case "todo/add":
       return { ...state, entities: state.entities.concat(payload) }
+    case "todo/completed":
+      let newTodos = state.entities.map(todo => {
+        if (todo.id === payload.id) {
+          todo.completed = !todo.completed
+          return todo
+        }
+        return todo
+      })
+      return { ...state, entities: newTodos }
   }
   return state
 }
 
 const Item = ({ todo }) => {
+  const dispatcher = useDispatch()
+
+  const handleClick = () => {
+    dispatcher({ type: "todo/completed", payload: todo })
+  }
   return (
-    <li>{todo.title}</li>
+    <li style={{ color: todo.completed ? "green" : "red" }} onClick={handleClick}>{todo.title}</li>
   )
 }
 
@@ -28,28 +42,35 @@ const App = () => {
   const state = useSelector(x => x)
   const [value, setValue] = useState("")
 
+
   const submit = e => {
     e.preventDefault()
 
     let todo = {
       id: Math.random().toString(36),
+      completed: false,
       title: value,
     }
     dispatcher({ type: "todo/add", payload: todo })
     setValue("")
   }
-  console.log({ state })
 
 
   return (
     <div>
+
       <form onSubmit={submit}>
         <input type="text" placeholder="Task description" value={value} onChange={e => setValue(e.target.value)} />
-        <input type="submit" />
       </form>
+
+      <button>Mostrar todos</button>
+      <button>Completados</button>
+      <button>Incompletos</button>
+
       <ul>
-        {state ? state.entities.map(todo => <Item key={todo.id} todo={todo} />) : null}
+        {state.entities.map(todo => <Item key={todo.id} todo={todo} />)}
       </ul>
+
     </div>
   )
 }
